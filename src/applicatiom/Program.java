91a580f14a796b39c5bf382a757b9a15b968e338
Sofia Xavier <sofiaxavierbraga087@gmail.com>
2026-07-18 21:14:54 -0300

    Special move - Promotion

--- src/applicatiom/Program.java
+++ src/applicatiom/Program.java
@@ -29,20 +29,26 @@ public class Program {
 				UI.clearScreen();
 				UI.printMatch(chessMatch,captured);
 				System.out.println();
-				System.out.println("Source: ");
+				System.out.print("Source: ");
 				ChessPosition source=UI.readChessPosition(sc);
 				
 				boolean[][]possibleMoves=chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(),possibleMoves);
 				System.out.println();
-				System.out.println("Target: ");
+				System.out.print("Target: ");
 				ChessPosition target=UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece=chessMatch.performeChessMove(source, target);
 				
 				if(capturedPiece !=null) {
 					captured.add(capturedPiece);
+				}
+				
+				if(chessMatch.getPromoted()!=null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q");
+					String type=sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
 				
 			} catch (ChessException e) {
